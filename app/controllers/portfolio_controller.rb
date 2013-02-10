d85995ee5d1e965c7d25d6c77ef807c0f1b4e3a5
Alex Stomp <stompy1208@gmail.com>
2013-02-10 17:45:29 -0600

    makes it presentable-ish

---
+++ app/controllers/portfolio_controller.rb
@@ -0,0 +1,15 @@
+class PortfolioController < ApplicationController
+
+	def chalkthemup
+	end
+
+	def bacc
+	end
+
+	def ete_print
+	end
+
+	def ete_web
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Alexstomp::Application.routes.draw do
   root :to => 'application#home'
 
   match 'blog' => 'application#blog', :as => :blog
+  match 'chalkthemup' => 'portfolio#chalkthemup', :as => :chalkthemup
+  match 'bacc' => 'portfolio#bacc', :as => :bacc
+  match 'ete_print' => 'portfolio#ete_print', :as => :ete_print
+  match 'ete_web' => 'portfolio#ete_web', :as => :ete_web
 
 end

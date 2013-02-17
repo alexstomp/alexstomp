3eeefcabb6702c711a1201d6bc4c193f09f6dba7
Alex Stomp <stompy1208@gmail.com>
2013-02-17 16:09:07 -0600

    update design

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Alexstomp::Application.routes.draw do
   
   root :to => 'application#home'
 
+  match 'resume' => 'application#resume', :as => :resume
   match 'blog' => 'application#blog', :as => :blog
   match 'chalkthemup' => 'portfolio#chalkthemup', :as => :chalkthemup
   match 'bacc' => 'portfolio#bacc', :as => :bacc
+  match 'aquamu' => 'portfolio#aquamu', :as => :aquamu
   match 'ete_print' => 'portfolio#ete_print', :as => :ete_print
   match 'ete_web' => 'portfolio#ete_web', :as => :ete_web
 
